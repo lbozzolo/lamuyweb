0719a4a5b7bb69667ec13f1644752f3900a05400
MAC <apple@MACs-MacBook-Pro.local>
2019-06-11 13:02:36 -0300

    Uso de url() helper en la construcción de la URL de pdf y cover image

--- app/Transformers/MagazineTransformer.php
+++ app/Transformers/MagazineTransformer.php
@@ -16,12 +16,12 @@ class MagazineTransformer extends TransformerAbstract
     public function transform(Magazine $magazine)
     {
         return [
-            'title' => (string)$magazine->title,
-            'description' => (string)$magazine->description,
+            'title' => ($magazine->title)? (string)$magazine->title : null,
+            'description' => ($magazine->description)? (string)$magazine->description : null,
             'number' => (int)$magazine->number,
             'date' => ($magazine->date)? (string)Carbon::parse($magazine->date)->format('m-Y') : null,
-            'url_pdf' => (string)storage_path('app/'.$magazine->url_pdf),
-            'url_cover' => (string)storage_path('app/covers/'.$magazine->url_cover),
+            'url_pdf' => ($magazine->url_pdf)? (string)url(storage_path('app/'.$magazine->url_pdf)) : null,
+            'url_cover' => ($magazine->url_cover)? (string)url(storage_path('app/covers/'.$magazine->url_cover)) : null,
         ];
     }
 }
